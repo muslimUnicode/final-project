d7d283df257c21acef360e9ff8871a474827297a
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-08 17:01:18 +0300

    delivery

---
+++ src/components/Delivery.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Delivery = () => {
+    return (
+        <div className='delivery'>
+            <h2>Условия доставки</h2>
+            <div className='accardion'>
+                <button></button>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Delivery;
