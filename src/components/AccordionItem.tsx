1c1c85b5a91b22414e3b40f2862bff6fd342da01
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-09 17:23:13 +0300

    accordeon add

---
+++ src/components/AccordionItem.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import ArrowDown from "../assets/icon/ArrowDown.svg";
+import ArrowUp from "../assets/icon/ArrowUp.svg";
+
+export interface PropsAccordeon {
+  faq: {
+    title: string;
+    content: string;
+  };
+  onToggle: any;
+  active: boolean;
+}
+
+const AccordionItem: FC<PropsAccordeon> = ({ faq, onToggle, active }) => {
+  const { title, content } = faq;
+
+  return (
+    <li className={`accordion_item ${active ? "active" : ""}`}>
+      <button className="button" onClick={onToggle}>
+        {title}
+        <span className="control">
+          {active ? <img src={ArrowUp} /> : <img src={ArrowDown} />}
+        </span>
+      </button>
+      <div className={`answer_wrapper ${active ? "open" : ""}`}>
+        <div className="content">{content}</div>
+      </div>
+    </li>
+  );
+};
+
+export default AccordionItem;
