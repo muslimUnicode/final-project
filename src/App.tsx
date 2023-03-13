c36a7ee3ada63b16275f2cafabeb5aa0770e0b1c
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-13 11:43:35 +0300

    pre-merge home

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
-<<<<<<< HEAD
 import Delivery from "./components/Delivery";
-=======
+import HomePage from "./pages/home-page/HomePage";
 import Footer from "./components/Footer";
->>>>>>> footer
 
 function App() {
     return (
         <div className="App">
-<<<<<<< HEAD
             <Delivery />
-=======
+            <HomePage/>
             <Footer />
->>>>>>> footer
         </div>
     )
 }
