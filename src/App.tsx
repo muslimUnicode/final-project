119d446077dc216997e5d1c6481089c9f29a4727
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-13 11:48:04 +0300

    merge home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import "./App.css";
-<<<<<<< HEAD
 import Delivery from "./components/Delivery";
 import HomePage from "./pages/home-page/HomePage";
 import Footer from "./components/Footer";
-=======
-import HomePage from "./pages/home-page/HomePage";
->>>>>>> home
 
 function App() {
     return (
         <div className="App">
-<<<<<<< HEAD
             <Delivery />
             <HomePage/>
             <Footer />
-=======
-            <HomePage/>
->>>>>>> home
         </div>
     )
 }
