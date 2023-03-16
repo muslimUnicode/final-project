098e205d6c7efd618ced2a0466f0a0314c249680
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-16 13:06:45 +0300

    added header

--- src/components/CardsList/CardItem/CardItem.tsx
+++ src/components/CardsList/CardItem/CardItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import pizza from "../../../assets/pizza.png";
-import buy from "../../../assets/buy.svg";
+import buy from "../../../assets/Buy.svg";
 
 const CardItem = () => {
   return (
