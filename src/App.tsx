a2bb4c0e03bb54f7d3ac6b931752690354e94b0f
Askhab <askhab@MacBook-Air-Askhab.local>
2023-03-16 12:34:13 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Routes>
+        {/* <Route path='/sign-up' element={< SignUp />}> */}
+      </Routes>
     </div>
   );
 }
